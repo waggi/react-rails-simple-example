173dffbeacfcee11347d6c2a8220d11959be0ba5
waggi <tatiana.vasilyeva@jetbrains.com>
2015-09-05 01:35:32 +0200

    Got ready for React

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require react
+//= require components
 //= require react_ujs
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
