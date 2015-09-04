3ddda3ca6ba6ec2ef0663a8af1557bf950a14ce2
waggi <tatiana.vasilyeva@jetbrains.com>
2015-09-05 01:31:58 +0200

    'Create' action was added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,19 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :contents)
+  end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
+  fixtures 'posts'
+  setup :init_post
+
   test 'get index action' do
     get :index
     assert_response :success
+  end
+
+  test 'should create post' do
+    post :create, post: @post.serializable_hash, :format => 'json'
+    assert_response :success
+    assert_not_nil JSON.parse(response.body)
+  end
+
+  def init_post
+    @post = posts(:one)
   end
 end
